7424f4b88398a4ec627f1e4ca8dc8f63d68683b9
Victor Nascimento <victor.nascimento@techlead.com.br>
2023-08-21 17:53:19 -0300

    Ajuste do form de serviços e da lista de serviços

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-list/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,23 @@ export class ServicoPrestadoService {
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
     return this.http.post<ServicoPrestado>(this.apiURL, servicoPrestado)
+  }
+
+  buscar(nome?: string, mes?:string) : Observable<ServicoPrestadoBusca[]> {
+    let httpParams = new HttpParams();
+
+    if (!nome) {
+      nome = "";
+    }
+
+    httpParams = httpParams.set("nome", nome);
+
+    if (mes) {
+      httpParams = httpParams.set("mes", mes);
+    }
+
+    const url = this.apiURL + "?" + httpParams.toString();    
+
+    return this.http.get<ServicoPrestadoBusca[]>(url);
   }
 }
--- src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
+++ src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-servico-prestado-list',
@@ -10,13 +13,30 @@ export class ServicoPrestadoListComponent {
   nome?: string;
   mes?: string;
   meses: number[];
+  lista?: ServicoPrestadoBusca[];
+  errors?: string [];
+  sucess?: boolean;
+  menssage?: string;
 
-  constructor() {
+  constructor(private servicoService : ServicoPrestadoService) {
     this.meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   }
 
   consultar() {
-    console.log(this.nome +" / "+this.mes);
+    this.servicoService.buscar(this.nome, this.mes).subscribe( response => {
+      this.lista = response;
+      
+      this.errors = [];
+      this.sucess = true;
+      if(this.lista.length <= 0) {
+        this.menssage = "Nenhum serviço encontado!";
+      } else {
+        this.menssage = "";
+      }
+    }, errorResponse => {
+      this.sucess = false;
+      this.errors = errorResponse.error.erros
+    })
     
   }
 
